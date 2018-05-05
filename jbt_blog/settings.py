bf8c54c4d96fbfcccc10a6d807a6523ca4e7177d
Eason <wangyisheng201412@gmail.com>
2018-05-05 16:45:58 +0800

    增加DEBUG状态判断

--- jbt_blog/settings.py
+++ jbt_blog/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+if DEBUG:
+    STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
