30f1fd0401e318ab5d8e95f4445d70fffe90904d
Eason <wangyisheng201412@gmail.com>
2018-05-07 21:27:13 +0800

    解决DEBUG=False时静态文件404问题

--- jbt_blog/settings.py
+++ jbt_blog/settings.py
@@ -127,6 +127,7 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
--- jbt_blog/urls.py
+++ jbt_blog/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from apps.blog import views
 from django.conf.urls import include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +38,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG})
+    ]
